23f1860c75c918ffdfb119a4b0819357821082ef
navneet <navneetsharma2302@gmail.com>
2023-08-29 00:20:41 +0530

    update

--- main.py
+++ main.py
@@ -6,31 +6,39 @@ def get_choices():
    options=["rock","paper","scissors"]
    computer_choice =random.choice(options)
    
-   choices={"player": player_choice,"computer":"computer_choice"}
+   choices={"player": player_choice,"computer":computer_choice}
 
    return choices
 
-def get_win(player,computer):
-   print ("you chose {player}, computer chose{computer}")
+def check_win(player,computer):
+   print (f"you chose {player}, computer chose {computer}")
    if player == computer:
     return "it's a tie!"
 
-   elif player == "rock" 
+   elif player == "rock":
      if computer =="sicssors":
-         return "you won!"
-       else:
-         return "you lost!"
-   elif player == "paper"
-       if computer =="rock":
-         return "you won!"
-       else:
-         return "you lost!"
-   elif player == "scissors"  
-         if computer =="paper":
-            return "you won!"
-         else:
-            return "you lost!"
-check_win("rock","paper")  
+       return "you won!"
+     else:
+       return "you lost!"
+   
+   elif player == "paper":
+     if computer =="rock":
+       return "you won!"
+     else:
+       return "you lost!"
+   
+   elif player == "scissors":
+     if computer =="paper":
+       return "you won!"
+     else:
+       return "you lost!"
+   
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print(result)
+
+# choices = {"player": "rock", "computer": "paper"}
+# p_choices = choices["computer"]
 
 
 
